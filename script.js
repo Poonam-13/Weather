31643ca92bde12ad669db68bce64024e6caebce9
Poonam Thakur <poonamthakur1377@gmail.com>
2023-05-07 12:10:30 +0530

    initial commit to github

---
+++ script.js
@@ -0,0 +1,38 @@
+const options = {
+    method: 'GET',
+    headers: {
+    'X-RapidAPI-Key': 'a3159050b2msh57005f2c0939d60p1df0b8jsn529204057f5b',
+    'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    .then(response => response.json())
+    .then((response) => {              
+        console.log(response)
+        
+        temp.innerHTML = response.temp
+        temp1.innerHTML = response.temp
+        feels_like.innerHTML = response.feels_like
+        humidity.innerHTML = response.humidity
+        humidity1.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed1.innerHTML = response.wind_speed
+        wind_degrees.innerHTML = response.wind_degrees
+        sunrise.innerHTML = response.sunrise
+        sunset.innerHTML = response.sunset
+        
+    })
+    .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+getWeather("Bhilai")
